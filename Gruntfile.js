1c3540fee006cff0d563d0cbbe84b7ad3016890c
ProjectSoft <projectsoft2009@yandex.ru>
2018-12-01 08:50:25 +0400

    init and release

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+	var gc = {
+		fontVers: '1.0.0',
+		tasks: [
+			'notify:watch',
+			'less',
+			'pug',
+			'notify:done'
+		]
+	};
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+	grunt.initConfig({
+		globalConfig : gc,
+		pkg : grunt.file.readJSON('package.json'),
+		less: {
+			css: {
+				options : {
+					compress: false,
+					ieCompat: false
+				},
+				files : {
+					'test/css/main.css' : [
+						'src/less/variables.less',
+						'src/less/main.less'
+					]
+				}
+			}
+		},
+		pug: {
+			files: {
+				options: {
+					pretty: '\t',
+					separator:  '\n'
+				},
+				files: {
+					"index.html": ['src/pug/index.pug']
+				}
+			}
+		},
+		notify: {
+			watch: {
+				options: {
+					title: "<%= pkg.name %> v<%= pkg.version %>",
+					message: 'Запуск',
+					image: __dirname+'\\src\\notify.png'
+				}
+			},
+			done: {
+				options: { 
+					title: "<%= pkg.name %> v<%= pkg.version %>",
+					message: "Успешно Завершено",
+					image: __dirname+'\\src\\notify.png'
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true,
+			},
+			compile: {
+				files: [
+					'src/**/*.*'
+				],
+				tasks: gc.tasks
+			}
+		}
+	});
+	grunt.registerTask('dev',		['watch']);
+	grunt.registerTask('default',	gc.tasks);
+}
